8749a15fa4b4f4ce873154cd74608e528e05aed6
mjg1123 <marquisgbusiness@gmail.com>
2019-11-27 17:42:14 -0500

    styled signin form and applied grid

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,41 +1,40 @@
 import React, { Component } from 'react';
 
-import {reduxForm, Field} from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
 
 class SignInForm extends Component {
-    state = {  }
     render() {
-        const { className, handleSubmit } = this.props; 
-        return ( 
-            <form onSubmit={handleSubmit} className={`${className} sign-in-form` }>
-                <Field className='sign-in-from__email' 
-                type='email' 
-                title='Email' 
-                placeholder ='Email'
-                name='email' 
-                component={FormInput} />
-                <Field className='sign-in-from__password' 
-                type='password' 
-                title='Password' 
-                placeholder ='Password'
-                name='password' 
-                component={FormInput} />
-                <div className="sign-in-form__line"></div>
-                <Field className='sign-in-from__login'
-                onClick = {() => console.log ('tryna submit')} 
-                type='submit' 
-                title='login'
-                name='login' 
-                component={FormButton} />
-                </form>
-         );
+        const { className, handleSubmit } = this.props;
+        return (
+            <form onSubmit={handleSubmit} className={`${className} sign-in-form`}>
+                <Field className='sign-in-form__email'
+                type='email'
+                title='Email'
+                placeholder='Email'
+                name='email'
+                component={FormInput}/>
+                <Field className='sign-in-form__password'
+                type='password'
+                title='Password'
+                placeholder='Password'
+                name='password'
+                component={FormInput}/>
+                <div className='sign-in-form__line'></div>
+                <Field className='sign-in-form__login'
+                onClick={() => console.log('tryna submit')}
+                type='submit'
+                title='Login'
+                name='login'
+                component={FormButton}/>
+            </form>
+        )
     }
 }
 
-SignInForm = reduxForm ({
+SignInForm = reduxForm({
     form: 'SignInForm'
 })(SignInForm);
- 
+
 export default SignInForm;
