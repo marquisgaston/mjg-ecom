e71e175f63360cca1b26cfe6251b290e4dbb87eb
mjg1123 <marquisgbusiness@gmail.com>
2019-11-27 21:50:23 -0500

    developed sign up form and modified form button for back function

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 
+import SignUpForm from "./signupForm";
+import PageTitle from '../pageTitle';
+
 class SignUp extends Component {
+
+    onSubmit = (fields) => {
+        console.log(fields)
+    }
+
     state = {  }
     render() { 
         return ( 
             <div className="sign-up">
-                sign-up
+                <PageTitle className='sign-up__page-title' title='Register' />
+                <SignUpForm onSubmit={this.onSubmit} className="sign-up__form" />
             </div>
          );
     }
