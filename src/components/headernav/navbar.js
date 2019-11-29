a9ae073faca966c64aa865e1f121360d960d4ead
mjg1123 <marquisgbusiness@gmail.com>
2019-11-29 12:26:03 -0500

    connected and header and navbar to redux store

--- src/components/headernav/navbar.js
+++ src/components/headernav/navbar.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 
+import { connect } from 'react-redux';
+
 class Navbar extends Component {
     state = {  }
     render() { 
         return ( 
             <div className ="navbar">
-
+                {
+                    this.props.navbarLinks.map((link, index) => {
+                        return (
+                            <a key={index} className='navbar__link' onClick={() => console.log('trying to switch to a tab')}>
+                                {link.title}
+                            </a>
+                        )
+                    })
+                }
             </div>
          );
     }
 }
+
+function mapStateToProps(state) {
+    const { navbarLinks } = state.headerNavbar;
+    return {
+        navbarLinks
+    }
+}
+
+Navbar = connect(mapStateToProps)(Navbar);
  
 export default Navbar
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { combineReducers } from 'redux';
 import {reducer as form} from 'redux-form';
 
-import HeaderNavbar from './headernavbarreducer';
+import headerNavbar from './headernavbarreducer';
 
 const rootReducer = combineReducers({
   form,
-  HeaderNavbar
+  headerNavbar
 });
 
 export default rootReducer;
