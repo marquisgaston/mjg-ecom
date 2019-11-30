caaf7edfa6055bc9ce672a451ec97b2ddfacb997
mjg1123 <marquisgbusiness@gmail.com>
2019-11-30 14:39:28 -0500

    bulit user reducer w/ fetchUserPurchases function

--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 
+import * as actions from '../../actions';
+import { connect } from 'react-redux';
+
 class Purchases extends Component {
+
+    componentDidMount() {
+        this.props.fetchUserPurchases();
+    }
+
     render() { 
         const { className } = this.props;
         return ( 
             <div className={`${className} purchases`}>
-                <h1>Purchases go here</h1>
+                {
+                    this.props.purchases.map(purchase => {
+                        return <div>{purchase.title}</div>
+                    })
+                }
             </div>
          );
     }
 }
+
+function mapStateToProps(state) {
+    const {purchases} = state.user;
+    return {purchases};
+}
+
+Purchases = connect (mapStateToProps, actions)(Purchases);
  
 export default Purchases;
